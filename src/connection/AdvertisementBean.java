a7fe53436d876117e90de0e39bf0779a417447ee
hetashah <heta.hets@gmail.com>
2013-11-08 23:22:24 -0800

    Few Changes

--- src/connection/AdvertisementBean.java
+++ src/connection/AdvertisementBean.java
@@ -3,11 +3,55 @@ package connection;
 public class AdvertisementBean {
 
 	
-	public int userid;
-	public int advertiseid;
-	public String itemname;
-	public String itemdesc;
-	public int itemprice;
-	public int itemquantity;
-	public String sellerinfo;
+	int userid;
+	int advertiseid;
+	String itemname;
+	String itemdesc;
+	int itemprice;
+	int itemquantity;
+	String sellerinfo;
+	
+	
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public int getAdvertiseid() {
+		return advertiseid;
+	}
+	public void setAdvertiseid(int advertiseid) {
+		this.advertiseid = advertiseid;
+	}
+	public String getItemname() {
+		return itemname;
+	}
+	public void setItemname(String itemname) {
+		this.itemname = itemname;
+	}
+	public String getItemdesc() {
+		return itemdesc;
+	}
+	public void setItemdesc(String itemdesc) {
+		this.itemdesc = itemdesc;
+	}
+	public int getItemprice() {
+		return itemprice;
+	}
+	public void setItemprice(int itemprice) {
+		this.itemprice = itemprice;
+	}
+	public int getItemquantity() {
+		return itemquantity;
+	}
+	public void setItemquantity(int itemquantity) {
+		this.itemquantity = itemquantity;
+	}
+	public String getSellerinfo() {
+		return sellerinfo;
+	}
+	public void setSellerinfo(String sellerinfo) {
+		this.sellerinfo = sellerinfo;
+	}
 }
